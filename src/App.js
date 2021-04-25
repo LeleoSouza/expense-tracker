62ef7dd882b88731a773f5013f0525554dfff1f3
LeleoSouza <leobrausa@hotmail.com>
2021-04-25 18:31:36 -0400

    new-components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
-
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Header />
+      <div className="cotainer">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+      </div>
+    </div>
+  );
 }
 
 export default App;
